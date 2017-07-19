48d355de0aeb967850732a9f1d9effdfa1cbb016
Muhammed Athimannil <hello@athimannil.com>
2017-07-19 13:48:15 +0200

    style for quote

--- src/js/components/Quote.js
+++ src/js/components/Quote.js
@@ -3,10 +3,12 @@ import React from "react";
 export class Quote extends React.Component {
   render() {
     return (
-      <blockquote className="">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <span>Authout Name</span>
-      </blockquote>
+      <div className="quote-wrap">
+        <blockquote className="">
+          <p className="quote">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <footer className="quote-author">Authout Name</footer>
+        </blockquote>
+      </div>
     );
   }
 }
