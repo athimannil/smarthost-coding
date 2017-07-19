ca4562250d59fb3fb0b9b5c27994fb7b68577463
Muhammed Athimannil <hello@athimannil.com>
2017-07-19 14:42:50 +0200

    avoid background flickering

--- src/js/components/Background.js
+++ src/js/components/Background.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const apiForPic = "https://pixabay.com/api/?key=5926034-701cedbed0d2d8e20edfb8b7a&image_type=photos&category=nature&editors_choice=true&safesearch=true&per_page=200&order=latest&pretty=true&page=2";
 
+const styles = {
+    transition: 'filter 0.5s linear'
+};
+
 export class Background extends React.Component {
 
   constructor(props) {
@@ -42,14 +46,30 @@ export class Background extends React.Component {
   };
 
   updateImage = () => {
+    this.onHide();
     setTimeout(this.updateImageState, 500);
+
+    setTimeout(function () {
+      this.onShow();
+    }.bind(this), 500);
+  };
+
+  onHide = () => {
+    this.setState({
+      filter: "opacity(0)"
+    });
+  };
+  onShow = () => {
+    this.setState({
+      filter: "opacity(1)"
+    });
   };
 
   render(){
     return(
       <section
         className="quote-wrap"
-        style={{ backgroundImage: `url(${this.state.backImage})` }}
+        style={{ ...styles, filter: this.state.filter, backgroundImage: `url(${this.state.backImage})` }}
       />
     )
   };
