ec532217cda607565c110093efc962e5245a8214
Muhammed Athimannil <hello@athimannil.com>
2017-07-19 13:39:21 +0200

    separate component

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import "../scss/main.scss";
 import React from "react";
 import { render } from "react-dom";
 
+import { Quote} from './components/Quote';
+
 class App extends React.Component {
   render() {
-    return <Background />;
+    return <Quote/>
   }
 }
 
