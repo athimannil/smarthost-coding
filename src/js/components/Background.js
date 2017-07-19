e39ddc7d40fe5781a9889e11feb78c847d3b509a
Muhammed Athimannil <hello@athimannil.com>
2017-07-19 14:12:10 +0200

    background image for screen

--- src/js/components/Background.js
+++ src/js/components/Background.js
@@ -3,7 +3,10 @@ import React from "react";
 export class Background extends React.Component {
   render(){
     return(
-      <section className="quote-wrap"/>
+      <section
+        className="quote-wrap"
+        style={{ backgroundImage: `url(${'https://pixabay.com/get/e830b80f21f6023ed95c4518b7494f94e577e1d104b0144195f4c07fa7efb2_640.jpg'})` }}
+      />
     )
   };
 }
